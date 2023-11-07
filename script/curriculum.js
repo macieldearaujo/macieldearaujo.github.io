a54f7a1f620684f8029afc294b1aeb1b44d79811
douglasmcl <douglasmacielaraujo@outlook.com>
2023-11-07 17:33:18 -0300

    Add the values from first page to an array

--- script/curriculum.js
+++ script/curriculum.js
@@ -5,11 +5,11 @@ const button_index = document.querySelector('.js-form-button-continue-index');
 button_index.addEventListener('click', () => {
     const name = document.querySelector('input[name="name"]').value;
     const email = document.querySelector('input[name="email"]').value;
-    const phone = document.querySelector('.input[name="phone"]').value;
-    const marital_status = document.querySelector('.select[name="maritalStatus"]').value;
-    const country = document.querySelector('.select[name="country"]').value;
-    const city =  document.querySelector('.input[name="city"]').value;
-    const neighborhood = document.querySelector('.input[name="neighborhood"]').value;
+    const phone = document.querySelector('.form-input[name="phone"]').value;
+    const marital_status = document.querySelector('.js-form-input-maritial-status').value;
+    const country = document.querySelector('.js-form-input-country').value;
+    const city =  document.querySelector('.form-input[name="city"]').value;
+    const neighborhood = document.querySelector('.js-form-input-neighborhood').value;
     
     form.push({
         personalInformation: {
