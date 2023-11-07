4708006a3f1b4a64cab566503c67a33497929dcd
douglasmcl <douglasmacielaraujo@outlook.com>
2023-11-07 17:09:19 -0300

    change the search of inputs, but returns a error

--- script/curriculum.js
+++ script/curriculum.js
@@ -3,13 +3,13 @@ import { form } from '../data/form.js'
 const button_index = document.querySelector('.js-form-button-continue-index');
 
 button_index.addEventListener('click', () => {
-    const name = document.querySelector('.js-form-input-name').value;
-    const email = document.querySelector('.js-form-input-email').value;
-    const phone = document.querySelector('.js-form-input-phone').value;
-    const marital_status = document.querySelector('.js-form-input-maritial-status').value;
-    const country = document.querySelector('.js-form-input-country').value;
-    const city =  document.querySelector('.js-form-input-city').value;
-    const neighborhood = document.querySelector('.js-form-input-neighborhood').value;
+    const name = document.querySelector('input[name="name"]').value;
+    const email = document.querySelector('input[name="email"]').value;
+    const phone = document.querySelector('.input[name="phone"]').value;
+    const marital_status = document.querySelector('.select[name="maritalStatus"]').value;
+    const country = document.querySelector('.select[name="country"]').value;
+    const city =  document.querySelector('.input[name="city"]').value;
+    const neighborhood = document.querySelector('.input[name="neighborhood"]').value;
     
     form.push({
         personalInformation: {
