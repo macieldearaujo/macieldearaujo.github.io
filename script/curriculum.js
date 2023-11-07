adddfcff3e7d611975682bfbcbea9e53acf92795
douglasmcl <douglasmacielaraujo@outlook.com>
2023-11-07 16:50:01 -0300

    Add types and names to inputs to autocomplete

--- script/curriculum.js
+++ script/curriculum.js
@@ -4,8 +4,25 @@ const button_index = document.querySelector('.js-form-button-continue-index');
 
 button_index.addEventListener('click', () => {
     const name = document.querySelector('.js-form-input-name').value;
+    const email = document.querySelector('.js-form-input-email').value;
+    const phone = document.querySelector('.js-form-input-phone').value;
+    const marital_status = document.querySelector('.js-form-input-maritial-status').value;
+    const country = document.querySelector('.js-form-input-country').value;
+    const city =  document.querySelector('.js-form-input-city').value;
+    const neighborhood = document.querySelector('.js-form-input-neighborhood').value;
+    
     form.push({
-        name
+        personalInformation: {
+            name,
+            email,
+            phone,
+            marital_status,
+        },
+        adress: {
+            country,
+            city,
+            neighborhood
+        }
     })
     console.log(form)
 })
