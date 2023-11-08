66d6ac709340694ca5de440a01c33c270f7e26a9
douglasmcl <douglasmacielaraujo@outlook.com>
2023-11-08 12:43:36 -0300

    Now you can have multiples courses

--- script/education revision.js
+++ script/education revision.js
@@ -1,30 +1,32 @@
-import { form, displayAlert, saveToStorage } from '../data/form.js'
+import { form, saveToStorage } from '../data/form.js'
 
 checkInput('education-revision');
 
-const course_name = form.courses.course_name;
-const course_institution = form.courses.course_institution;
-const course_begin_month = form.courses.course_begin_month;
-const course_begin_year = form.courses.course_begin_year;
-const course_end_month = form.courses.course_end_month;
-const course_end_year = form.courses.course_end_year;
-
-const container = document.querySelector('.form-container-course')
+const container = document.querySelector('.form-container-course');
 let courseHTML = '';
 
-courseHTML += `
-    <section class="form-section">
-    <h2 class="form-title-topic">Revise o seu histórico escolar:</h2>
-    <section class="form-container-revision">
-        <h3 class="form-title-revision-1">${course_name}</h3>
-        <button class="bin-img">
-            <img src="img/bin.png" style="width: 24px;">
-        </button>
-        <p class="form-title-revision-2">${course_institution}</p>
-        <p class="form-title-revision-3">${course_begin_month}/${course_begin_year} a ${course_end_month}/${course_end_year}</p>
-    </section>
-    `
-container.innerHTML = courseHTML
+form.courses.forEach((course) => {
+    const course_name = course.course_name;
+    const course_institution = course.course_institution;
+    const course_begin_month = course.course_begin_month;
+    const course_begin_year = course.course_begin_year;
+    const course_end_month = course.course_end_month;
+    const course_end_year = course.course_end_year;
+
+    courseHTML += `
+        <section class="form-section">
+        <section class="form-container-revision">
+            <h3 class="form-title-revision-1">${course_name}</h3>
+            <button class="bin-img">
+                <img src="img/bin.png" style="width: 24px;">
+            </button>
+            <p class="form-title-revision-2">${course_institution}</p>
+            <p class="form-title-revision-3">${course_begin_month}/${course_begin_year} a ${course_end_month}/${course_end_year}</p>
+        </section>
+        ` 
+})
+
+container.innerHTML = courseHTML 
 
 const continueButton = document.querySelector('.js-form-button-add-education-revision');
 
